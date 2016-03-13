8823436133c76c866205afbb13aab59ab9133353
Cesar Espinosa <cesarjr.espinosa@gmail.com>
2016-03-13 01:47:05 -0600

    Second commit (the real one)

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<title>SalvAppMe</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+		<link rel="shorcut icon" href="assets/img/icon.ico">
+	</head>
+	<body>
+		<h1> Hola </h1>
+	</body>
+</html>
