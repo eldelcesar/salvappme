06e4e546e6e99ffd9ce72f28d40c893644548a43
Cesar Espinosa <cesarjr.espinosa@gmail.com>
2016-03-13 10:49:28 -0600

    Fourth commit

--- assets/php/salvappme.php
+++ assets/php/salvappme.php
@@ -7,7 +7,7 @@ class SalvAppMe{
 	//------------System Information--------------
 
 	//Nombre de la Base de Datos
-	static public $db_name 		= "salvappme";
+	static public $db_name 		= "SALVAPPME";
 	static public $db_user 		= "root";//Configuracion básica de MAMP
 	static public $db_password	= "root";//Configuracion básica de MAMP
 	// real pass c3ntr4ld3c4mbio
@@ -21,8 +21,8 @@ class SalvAppMe{
 	//static public $db_user 		= "";
 	//static public $db_password	= "";
 
-	static public $domain = "http://localhost/salvappme/";
-	//static public $domain = "http://salvapme.tk/";
+	//static public $domain = "http://localhost/salvappme/";
+	static public $domain = "http://salvapme.tk/";
 
 }
 ?>
